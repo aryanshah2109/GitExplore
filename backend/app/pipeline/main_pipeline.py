6dc3e9e555cb515d344605102f0fce5c413fa4c3
Aryan Shah <aryanrshah2109@gmail.com>
2026-05-13 22:19:11 +0530

    Updated main pipeline

--- backend/app/pipeline/main_pipeline.py
+++ backend/app/pipeline/main_pipeline.py
@@ -4,6 +4,9 @@ from backend.app.chunking.ast_traverser import ASTTraverser
 repo_url = "https://github.com/aryanshah2109/FraudDetect"
 branch = "main"
 
+# repo_url = "https://github.com/aryanshah2109/DSA-Codes"
+# branch = "master"
+
 ingestion_pipeline = IngestionPipeline(
     repo_url=repo_url,
     branch_name=branch
