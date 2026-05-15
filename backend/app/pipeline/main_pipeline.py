1ed030e4e93f8b9a83ecef2e1b03b6404e131f38
Aryan Shah <aryanrshah2109@gmail.com>
2026-05-15 19:02:29 +0530

    Updated main pipeline

--- backend/app/pipeline/main_pipeline.py
+++ backend/app/pipeline/main_pipeline.py
@@ -1,5 +1,8 @@
 from backend.app.pipeline.ingestion_pipeline import IngestionPipeline
 from backend.app.chunking.ast_traverser import ASTTraverser
+from backend.app.embeddings.storage import StorageToQdrant
+
+import json
 
 repo_url = "https://github.com/aryanshah2109/FraudDetect"
 branch = "main"
@@ -16,4 +19,17 @@ manifest_path = ingestion_pipeline.run()
 
 traverser = ASTTraverser(manifest_path)
 
-traverser.symbol_extraction()
+all_symbol_paths = traverser.symbol_extraction()
+
+all_chunks = []
+
+for path in all_symbol_paths:
+
+    with open(path, "r", encoding="utf-8") as f:
+        chunks = json.load(f)
+
+    all_chunks.extend(chunks)
+
+qdrant_storage_object = StorageToQdrant()
+
+qdrant_storage_object.store_chunks(all_chunks=all_chunks)
